c79530377b17b567bddde062e8248faeacec2301
Kelly Mears <code@kellymears.me>
2013-06-20 23:06:25 -0300

    Update index.php
    updates

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $convio_survival->process_method();
 
 <body>
 
-  <div class="row">
+	<div class="row">
 		<div class="large-12 columns">
 			<h1>Convio Survival Toolkit</h1>
 			<h2 class="subheader">A PHP Utility to Help Make Convio Suck Less</h2>
@@ -55,6 +55,9 @@ $convio_survival->process_method();
 			<div class="large-3 columns">
 				<div class="row">
 					<div class="panel callout radius">
+						<p>for free on <a style="color: white; text-decoration: underline;" href="https://github.com/kellymears/convio-survival-kit">github</a></p>
+					</div>
+					<div class="panel callout radius">
 						<p>requires: simpledom, convio-open-api</p>
 					</div>
 				</div>
@@ -68,26 +71,31 @@ $convio_survival->process_method();
 
 				<div class="panel">
 					<h4>add_to_group</h4>
-					<p>Problem it solves: easily adding a fucking person to a fucking group.</p>
-					<p><kbd>convio-survival-toolkit.php?<strong>method=add_to_group</strong>&<strong>group_id={group_id}</strong>&<strong>email={email}</strong></kbd></p>
+					<p>Problem it solves: adding a person to a roup.</p>
+					<p><kbd>?<strong>method=add_to_group</strong>&<strong>group_id={group_id}</strong>&<strong>email={email}</strong></kbd></p>
 				</div>
 
 				<div class="panel">
 					<h4>remove_from_group</h4>
-					<p>Problem it solves: easily removing a fucking person to a fucking group.</p>
-					<kbd>convio-survival-toolkit.php?<strong>method=remove_from_group</strong>&<strong>group_id={group_id}</strong>&<strong>email={email}</strong></kbd></p>
+					<p>Problem it solves: removing a person from a group.</p>
+					<kbd>?<strong>method=remove_from_group</strong>&<strong>group_id={group_id}</strong>&<strong>email={email}</strong></kbd></p>
 				</div>
 
 				<div class="panel">
 					<h4>update_email</h4>
 					<p>Problem it solves: updating someone's email.</p>
-					<kbd>convio-survival-toolkit.php?<strong>method=update_email</strong>&<strong>cons_id={constituent_id}</strong>&<strong>email={email}</strong></kbd></p>
+					<kbd>?<strong>method=update_email</strong>&<strong>cons_id={constituent_id}</strong>&<strong>email={email}</strong></kbd></p>
 				</div>
 
 				<div class="panel">
 					<h4>get_congressperson</h4>
 					<p>ever wanted easy contact information for a representative? all you need is a zip code.</p>
-					<kbd>convio-survival-toolkit.php?<strong>method=get_congressperson</strong>&<strong>zip={zip}</strong></kbd></p>
+					<kbd>?<strong>method=get_congressperson</strong>&<strong>zip={zip}</strong></kbd></p>
+				</div>
+
+				<div class="panel">
+					<h4>take_action</h4>
+					<kbd>?method=take_action&alert_id={alert_id}&email={email}&phone={phone}</kbd></p>
 				</div>
 
 			</div>
@@ -98,13 +106,13 @@ $convio_survival->process_method();
 				
 				<div class="panel">
 					<h4>add_to_group</h4>
-					<p>Problem it solves: easily adding a fucking person to a fucking group. <strong>In code.</strong></p>
+					<p>Problem it solves: easily adding a friggin person to a flippin group. <strong>In code.</strong></p>
 					<p><kbd>$convio_survival->add_to_group($email, $group_id);</kbd></p>
 				</div>
 
 				<div class="panel">
 					<h4>remove_from_group</h4>
-					<p>Problem it solves: easily removing a fucking person from a fucking group. <strong>In code.</strong></p>
+					<p>Problem it solves: easily removing a person from a g-d group. <strong>In code.</strong></p>
 					<p><kbd>$convio_survival->remove_from_group($email, $group_id);</kbd></p>
 				</div>
 
@@ -114,26 +122,43 @@ $convio_survival->process_method();
 				</div>
 
 				<div class="panel">
+					<h4>take_action</h4>
+					<p><kbd>$convio_survival->take_action($alert_id,$email,$phone);</kbd></p>
+				</div>
+
+				<div class="panel">
+					<h4>get_action_count</h4>
+					<p>show off everywhere</p>
+					<p><kbd>$convio_survival->get_action_count($alert_id);</kbd></p>
+					<p><?php echo $convio_survival->get_action_count(723); ?> actions on <a href="https://secure3.convio.net/sdc/site/Advocacy?cmd=display&page=UserAction&id=723">Renew the Burmese Freedom and Democracy Act </a></p>
+					<p><?php echo $convio_survival->get_action_count(707); ?> actions on <a href="https://secure3.convio.net/sdc/site/Advocacy?cmd=display&page=UserAction&id=707">Tell your Congressperson: Support the Sudan Peace, Security and Accountability Act</a></p>
+					<p><?php echo $convio_survival->get_action_count(701); ?> actions on <a href="https://secure3.convio.net/sdc/site/Advocacy?cmd=display&page=UserAction&id=701">Stop U.S. Support for Syrian Weapons </a></p>
+				</div>
+
+				<div class="panel">
 					<h4>get_congressperson</h4>
 					<p><kbd>$convio_survival->get_congressperson($zip);</kbd></p>
 					<p>so, for example<br /><kbd>$convio_survival->get_congressperson(26241);</kbd> returns:</p>
 					<pre><?php print_r($convio_survival->get_congressperson(26241)); ?></pre>
+					<p>and <br /><kbd>$convio_survival->get_congressperson(90210);</kbd> returns:</p>
+					<pre><?php print_r($convio_survival->get_congressperson(90210)); ?></pre>
+				</div>
+
+				<div class="panel">
+					<h4>return_errors</h4>
+					<p>something failing with the survival kit?</p>
+					<p><kbd>$convio_survival->return_errors();</kbd></p>
+					<pre><?php print_r($convio_survival->return_errors()); ?></pre>
+				</div>
+
+				<div class="panel">
+					<h4>return_messages</h4>
+					<p>want to know what's going on inside the kit for dev?</p>
+					<p><kbd>$convio_survival->return_messages();</kbd></p>
+					<pre><?php print_r($convio_survival->return_messages()); ?></pre>
 				</div>
 				
 
-			</div>
-		</div>
-		<div class="row">
-			<div class="large-12 columns">
-				<h2>Output</h2>
-				<p>This is a live page, try constructing an api call and see the output below.</p>
-				<?php 
-					echo "<h2>Messages</h2>";
-					$convio_survival->return_messages();
-					echo "<h2>Errors</h2>";
-					$convio_survival->return_errors();
-
-				?>
 			</div>
 		</div>
 
